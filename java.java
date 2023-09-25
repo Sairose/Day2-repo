f29b87986afe5297f4f8012d00fc340fdca68395
Sairose <145376804+Sairose@users.noreply.github.com>
2023-09-25 11:56:19 +0545

    added java.java file to Day2-rep

---
+++ java.java
@@ -0,0 +1,18 @@
+class java{
+    void div(int a){
+        if(a==0){
+            throw new Excep();
+        }else{
+            System.out.println(a);
+        }
+    }
+    public static void main(String args[]){
+        java j= new java();
+        j.div(0);
+    }
+}
+class Excep extends ArithmeticException{
+    Excep(){
+        super("divided by zero");
+    }
+}
